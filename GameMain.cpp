b3b90f1d401073dec5fa2c705ab3ec32c621b5cf
WIN10 <galsuchetzky@gmail.com>
2019-08-03 00:02:59 +0300

    testing commiting

---
+++ GameMain.cpp
@@ -0,0 +1,5 @@
+//
+// Created by WIN10 on 8/2/2019.
+//
+
+#include "GameMain.h"
---
+++ GameMain.h
@@ -0,0 +1,14 @@
+//
+// Created by WIN10 on 8/2/2019.
+//
+
+#ifndef BRICKBREAKER_GAMEMAIN_H
+#define BRICKBREAKER_GAMEMAIN_H
+
+
+class GameMain {
+
+};
+
+
+#endif //BRICKBREAKER_GAMEMAIN_H
